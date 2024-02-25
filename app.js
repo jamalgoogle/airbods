564d1103323de65c22b95d497c0dd7dab40aab0e
Jamal Google <bassemgamal246@gmail.com>
2024-02-25 20:55:03 +0200

    done
    done

---
+++ app.js
@@ -0,0 +1,58 @@
+// let nextButton = document.getElementById('next');
+// let prevButton = document.getElementById('prev');
+let carousel = document.querySelector('.carousel');
+let listHTML = document.querySelector('.carousel .list');
+let seeMoreButtons = document.querySelectorAll('.seeMore');
+let backButton = document.getElementById('back');
+
+// nextButton.onclick = function(){
+//     showSlider('next');
+// }
+
+document.onkeydown = function(e){
+    e = e || window.event;
+    switch (e.which || e.keyCode){
+        case 39: showSlider('next');
+        break;
+        case 37: showSlider('prev');
+        break;
+    }
+}
+
+// document.addEventListener('keydown', function(){showSlider('next')})
+// prevButton.onclick = function(){
+//     showSlider('prev');
+// }
+let unAcceppClick;
+
+const showSlider = (type) => {
+    // nextButton.style.pointerEvents = 'none';
+    // prevButton.style.pointerEvents = 'none';
+    carousel.classList.remove('next', 'prev');
+    let items = document.querySelectorAll('.carousel .list .item');
+    if(type === 'next'){
+        listHTML.appendChild(items[0])
+        carousel.classList.add('next');
+    }else{
+        listHTML.prepend(items[items.length -1]);
+        carousel.classList.add('prev');
+    }
+    clearTimeout(unAcceppClick);
+    unAcceppClick = setTimeout(()=>{
+        nextButton.style.pointerEvents = 'auto';
+        prevButton.style.pointerEvents = 'auto';
+    }, 2000)
+}
+seeMoreButtons.forEach((button) => {
+    button.onclick = function(){
+        carousel.classList.remove('next', 'prev');
+        carousel.classList.add('showDetail');
+        backButton.classList.add('showbtn')
+        backButton.classList.remove('hidebtn')
+    }
+});
+backButton.onclick = function(){
+    carousel.classList.remove('showDetail');
+    backButton.classList.remove('showbtn')
+    backButton.classList.add('hidebtn')
+};
